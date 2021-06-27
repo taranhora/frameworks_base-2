Evolution-X-AOSP/frameworks_base

Implement animated FOD icon picker
* Move FOD icon picker code to FODIconView.java

Co-authored-by: Stallix <joey@evolution-x.org>
Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/systemui/biometrics/FODIconView.java
@@ -36,6 +36,41 @@ public class FODIconView extends ImageView {
     private int mSize;
     private final WindowManager mWindowManager;
 
+    private int mSelectedAnim;
+    private final int[] ANIMATION_STYLES_NAMES = {
+        R.drawable.fod_icon_aod_anim
+    };
+
+    private int mSelectedIcon;
+    private final int[] ICON_STYLES = {
+        R.drawable.fod_icon_default,
+        R.drawable.fod_icon_default_0,
+        R.drawable.fod_icon_default_1,
+        R.drawable.fod_icon_default_2,
+        R.drawable.fod_icon_default_3,
+        R.drawable.fod_icon_default_4,
+        R.drawable.fod_icon_default_5,
+        R.drawable.fod_icon_arc_reactor,
+        R.drawable.fod_icon_cpt_america_flat,
+        R.drawable.fod_icon_cpt_america_flat_gray,
+        R.drawable.fod_icon_dragon_black_flat,
+        R.drawable.fod_icon_evo1,
+        R.drawable.fod_icon_glow_circle,
+        R.drawable.fod_icon_neon_arc,
+        R.drawable.fod_icon_neon_arc_gray,
+        R.drawable.fod_icon_neon_circle_pink,
+        R.drawable.fod_icon_neon_triangle,
+        R.drawable.fod_icon_paint_splash_circle,
+        R.drawable.fod_icon_rainbow_horn,
+        R.drawable.fod_icon_shooky,
+        R.drawable.fod_icon_spiral_blue,
+        R.drawable.fod_icon_sun_metro,
+        R.drawable.fod_icon_scratch_pink_blue,
+        R.drawable.fod_icon_spiral,
+        R.drawable.fod_icon_fire_ice_ouroboros,
+        R.drawable.fod_icon_transparent
+    };
+
     public FODIconView(Context context, int i, int i2, int i3) {
         super(context);
         this.mPositionX = i2;
@@ -58,12 +93,21 @@ public class FODIconView extends ImageView {
         boolean z = Settings.System.getInt(getContext().getContentResolver(), "fod_icon_animation", 0) != 0;
         this.mIsFODIconAnimated = z;
         if (z) {
-            setBackgroundResource(R.drawable.fod_icon_anim);
+            setBackgroundResource(ANIMATION_STYLES_NAMES[mSelectedAnim]);
             this.iconAnim = (AnimationDrawable) getBackground();
         } else {
-            setImageResource(R.drawable.fod_icon_default);
+            setImageResource(ICON_STYLES[mSelectedIcon]);
         }
         hide();
+
+        update(z);
+    }
+
+    public void update(boolean isEnabled) {
+        mSelectedIcon = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.FOD_ICON, 0);
+        mSelectedAnim = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.FOD_ICON_ANIM_TYPE, 0);
     }
 
     public void hide() {
@@ -97,12 +141,12 @@ public class FODIconView extends ImageView {
         this.mIsFODIconAnimated = z;
         if (z) {
             setImageResource(0);
-            setBackgroundResource(R.drawable.fod_icon_anim);
+            setBackgroundResource(ANIMATION_STYLES_NAMES[mSelectedAnim]);
             this.iconAnim = (AnimationDrawable) getBackground();
             return;
         }
         setBackgroundResource(0);
-        setImageResource(R.drawable.fod_icon_default);
+        setImageResource(ICON_STYLES[mSelectedIcon]);
     }
 
     public void setIsKeyguard(boolean z) {

taranhora/frameworks_base-2

packages/SystemUI/src/com/android/systemui/biometrics/FODIconView.java
@@ -36,6 +36,42 @@ public class FODIconView extends ImageView {
     private int mSize;
     private final WindowManager mWindowManager;
 
+    private int mSelectedAnim;
+    private final int[] ANIMATION_STYLES_NAMES = {
+        R.drawable.fod_icon_aod_anim
+    };
+
+    private int mSelectedIcon;
+    private final int[] ICON_STYLES = {
+        R.drawable.fod_icon_default,
+        R.drawable.fod_icon_default_0,
+        R.drawable.fod_icon_default_1,
+        R.drawable.fod_icon_default_2,
+        R.drawable.fod_icon_default_3,
+        R.drawable.fod_icon_default_4,
+        R.drawable.fod_icon_default_5,
+        R.drawable.fod_icon_arc_reactor,
+        R.drawable.fod_icon_cpt_america_flat,
+        R.drawable.fod_icon_cpt_america_flat_gray,
+        R.drawable.fod_icon_dragon_black_flat,
+        R.drawable.fod_icon_glow_circle,
+        R.drawable.fod_icon_neon_arc,
+        R.drawable.fod_icon_neon_arc_gray,
+        R.drawable.fod_icon_neon_circle_pink,
+        R.drawable.fod_icon_neon_triangle,
+        R.drawable.fod_icon_paint_splash_circle,
+        R.drawable.fod_icon_rainbow_horn,
+        R.drawable.fod_icon_shooky,
+        R.drawable.fod_icon_spiral_blue,
+        R.drawable.fod_icon_sun_metro,
+        R.drawable.fod_icon_scratch_pink_blue,
+        R.drawable.fod_icon_scratch_red_blue,
+        R.drawable.fod_icon_fire_ice_ouroboros,
+        R.drawable.fod_icon_light,
+        R.drawable.fod_icon_gxzw,
+        R.drawable.fod_icon_transparent
+    };
+
     public FODIconView(Context context, int i, int i2, int i3) {
         super(context);
         this.mPositionX = i2;
@@ -58,12 +94,21 @@ public class FODIconView extends ImageView {
         boolean z = Settings.System.getInt(getContext().getContentResolver(), "fod_icon_animation", 0) != 0;
         this.mIsFODIconAnimated = z;
         if (z) {
-            setBackgroundResource(R.drawable.fod_icon_anim);
+            setBackgroundResource(ANIMATION_STYLES_NAMES[mSelectedAnim]);
             this.iconAnim = (AnimationDrawable) getBackground();
         } else {
-            setImageResource(R.drawable.fod_icon_default);
+            setImageResource(ICON_STYLES[mSelectedIcon]);
         }
         hide();
+
+        update(z);
+    }
+
+    public void update(boolean isEnabled) {
+        mSelectedIcon = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.FOD_ICON, 0);
+        mSelectedAnim = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.FOD_ICON_ANIM_TYPE, 0);
     }
 
     public void hide() {
@@ -97,12 +142,12 @@ public class FODIconView extends ImageView {
         this.mIsFODIconAnimated = z;
         if (z) {
             setImageResource(0);
-            setBackgroundResource(R.drawable.fod_icon_anim);
+            setBackgroundResource(ANIMATION_STYLES_NAMES[mSelectedAnim]);
             this.iconAnim = (AnimationDrawable) getBackground();
             return;
         }
         setBackgroundResource(0);
-        setImageResource(R.drawable.fod_icon_default);
+        setImageResource(ICON_STYLES[mSelectedIcon]);
     }
 
     public void setIsKeyguard(boolean z) {